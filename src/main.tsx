72fbb6c9f92586322e883b3e70b12ee48d36830a
Leodyver Semilla <leodyversemilla07@gmail.com>
2026-03-10 14:00:14 +0800

    fix: correct App import casing for Linux builds

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import App from "./app.tsx"
+import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { Toaster } from "@/components/ui/sonner"
 
